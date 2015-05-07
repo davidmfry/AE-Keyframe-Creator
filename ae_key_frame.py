62b1d6ce362b59a7dbc83647f0a38d173a52cc65
David Fry <david.fry.tv@gmail.com>
2015-05-07 07:34:50 -0700

    Recreated the keyframe class

--- ae_key_frame.py
+++ ae_key_frame.py
@@ -3,23 +3,12 @@ __author__ = 'david'
 
 
 class AE_Keyframe():
+    frame = 0
+    base_keyframe_type = ""
+    key_frame_type = ""
+    keyframe_data = []
 
-    units_per_sec = 0.0             # Also frame per second for the video clip
-    src_width = 0                   # Video width IE: 1920
-    src_height = 0                  # Video height IE: 1080
-    src_pixel_aspect_ratio = 0      # Normal aspect would be 1
-    comp_pixel_aspect_ratio = 0     # Normal aspect would be 1
-
-    def __init__(self, units_per_sec, src_width, src_height, src_pixel_aspect_ratio=1, comp_pixel_aspect_ratio=1):
-        self.units_per_sec = units_per_sec
-        self.src_width = src_width
-        self.src_height = src_height
-        self.src_pixel_aspect_ratio = src_pixel_aspect_ratio
-        self.comp_pixel_aspect_ratio = comp_pixel_aspect_ratio
-
-    def make_pos_keyframe(self, frame, x_pos, y_pos, z_pos):
-        return "{frame} {x_pos} {y_pos} {z_pos}".format(frame=frame, x_pos=x_pos, y_pos=y_pos, z_pos=z_pos)
-
-    def make_rot_keyframe(self, frame, degree):
-        return "{frame} {degree}".format(frame=frame, degree=degree)
-
+    def __init__(self, frame, base_keyframe_type, key_frame_type):
+        self.frame = frame
+        self.base_keyframe_type = base_keyframe_type
+        self.key_frame_type = key_frame_type
--- keyframe_format.py
+++ keyframe_format.py
@@ -1,14 +1,30 @@
 __author__ = 'david'
 import ae_key_frame
 
-keyframe_boiler_plate = "Adobe After Effects 8.0 Keyframe Data"
-keyframe_end_line = "End of Keyframe Data"
+# keyframe_boiler_plate = "Adobe After Effects 8.0 Keyframe Data"
+# keyframe_end_line = "End of Keyframe Data"
+#
+# keyframe_list = []
+#
+# def add_keyframe_to_list(keyframe_type):
+#
+#
+#
+# def export_keyframe_data():
+#     pass
 
-keyframe_list = []
+class Keyframe_Formatter():
 
-def add_keyframe_to_list(keyframe_type):
+    units_per_sec = 0.0             # Also frame per second for the video clip
+    src_width = 0                   # Video width IE: 1920
+    src_height = 0                  # Video height IE: 1080
+    src_pixel_aspect_ratio = 0      # Normal aspect would be 1
+    comp_pixel_aspect_ratio = 0     # Normal aspect would be 1
 
+    def __init__(self, units_per_sec, src_width, src_height, src_pixel_aspect_ratio=1, comp_pixel_aspect_ratio=1):
+        self.units_per_sec = units_per_sec
+        self.src_width = src_width
+        self.src_height = src_height
+        self.src_pixel_aspect_ratio = src_pixel_aspect_ratio
+        self.comp_pixel_aspect_ratio = comp_pixel_aspect_ratio
 
-
-def export_keyframe_data():
-    pass
